05dc888ccf04dca49ad91614446ec8eeb0a30d67
Kaio <trongleele@gmail.com>
2025-05-14 11:25:11 +0700

    git init app

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const list: string = 'a,b,c,d,e,f,g'
+
+const handleList = (v: string): string => {
+  return v.split(',').join('-')
+}
+
+const attribute = 'href'
+const url: string = 'https://google.com'
+
+const number = 123 // Nếu là reactive => const number = ref(123)
+</script>
+
+<template>
+  <button>
+    {{ number }}
+  </button>
+  <div class="flex gap-4">
+    <div :id="handleList(list)">11</div>
+    <a :[attribute]="url">Google</a>
+  </div>
+</template>
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup>
+const msg = 'Hello world'
+</script>
+
+<template>
+  <div>
+    <h1 class="text-red-500 text-4xl">Vue js</h1>
+    <span>Message: {{ msg }}</span>
+  </div>
+</template>
+
